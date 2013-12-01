1ae5977de214731e31349f5d5fa17e923b643a18
cithraidt <lwcloneu2@cithraidt.de>
2013-12-01 23:13:19 +0100

    fixed typo(s)

--- win32/driver/src/ledwiz.cpp
+++ win32/driver/src/ledwiz.cpp
@@ -2,7 +2,7 @@
  * LWCloneU2
  * Copyright (C) 2013 Andreas Dittrich <dittrich@cithraidt.de>
  *
- * This program is free software; you can redistribute it and/or modifyit under the terms of the
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
  * GNU General Public License as published by the Free Software Foundation;
  * either version 2 of the License, or (at your option) any later version.
  *
