689f537ca665cef744e0ff27ecb111ed14017cdd
cithraidt <lwcloneu2@cithraidt.de>
2014-02-02 10:30:48 +0100

    adjusted polling interval

--- firmware/descriptors.c
+++ firmware/descriptors.c
@@ -473,7 +473,7 @@ static const USB_Descriptor_Configuration_t PROGMEM ConfigurationDescriptor =
 		.EndpointAddress        = PANEL_EPADDR,
 		.Attributes             = (EP_TYPE_INTERRUPT | ENDPOINT_ATTR_NO_SYNC | ENDPOINT_USAGE_DATA),
 		.EndpointSize           = PANEL_EPSIZE,
-		.PollingIntervalMS      = 1
+		.PollingIntervalMS      = 2
 	},
 	#endif
 
